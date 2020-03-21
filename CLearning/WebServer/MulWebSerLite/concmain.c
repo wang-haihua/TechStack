36e1626997be8eb1bc81e520b4486df98775a203
HaihuaHaihua <HeVerWong@163.com>
2020-03-21 20:21:41 +0800

    add some code

--- CLearning/WebServer/MulWebSerLite/concmain.c
+++ CLearning/WebServer/MulWebSerLite/concmain.c
@@ -5,6 +5,13 @@
  *套接字地址的结构体类型
  ************************/
 
+void command(void){
+	char buf[MAXLINE];
+	if(!fgets(buf, MAXLINE, stdin))
+		exit(0);
+	printf("%s",buf);
+}
+
 /*
  * struct sockaddr_in {
  *	uint16_t sin_family; //协议簇AF_INET
@@ -23,7 +30,7 @@ int main(int argc, const char *argv[])
 {
     int listenfd, connfd, port, clientlen;
     struct sockaddr_in clientaddr;
-	static pool pool;
+	fd_set read_set, ready_set;
 
 	/* 检查命令行参数，并将第1个参数作为端口号port */
     if (argc != 2) {
@@ -32,22 +39,34 @@ int main(int argc, const char *argv[])
     }
     port = atoi(argv[1]);
 
+	/* 在SIGCHLD处理程序中回收操作系统分配给CGI子进程的资源 */
+	/*
+	if(Signal(SIGCHLD, sigchild_handler) == SIG_ERR)
+	{
+		unix_error("signal child handler error");
+	}
+	*/
+
     listenfd = Open_listenfd(port); //将输入port与服务器套接字地址建立联系，形成监听套接字
-	init_pool(listenfd, &pool);
+
+	FD_ZERO(&read_set);
+	FD_SET(STDIN_FILENO, &read_set);
+	FD_SET(listenfd, &read_set);
 
 	/*无限服务器循环，不断地接收连接请求*/
     while (1) {
-		pool.ready_set = pool.read_set;
-		pool.nready = Select(pool.maxfd+1, &pool.read_set, NULL, NULL, NULL);
-
-		if(FD_ISSET(listenfd, &pool.ready_set)){
+		ready_set = read_set;
+		Select(listenfd+1, &ready_set, NULL, NULL, NULL);
+		if(FD_ISSET(STDIN_FILENO, &ready_set))
+			command();
+		if(FD_ISSET(listenfd, &ready_set)){
 			clientlen = sizeof(clientaddr);
-			connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);//使用Accept()函数等待客户端连接
-			add_client(connfd, &pool);
+			connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);//使用Accept()函数等待客户端连接	
+			handle_http_trans(connfd);
+			Close(connfd);
 		}
-	
-		check_clients(&pool);		
     }
 }
+
 
 /* $end main.c */
--- CLearning/WebServer/MulWebSerLite/err_handle_wra.c
+++ CLearning/WebServer/MulWebSerLite/err_handle_wra.c
@@ -25,19 +25,37 @@ void unix_error(char *msg)
 
 /*
  * 
- * unix_error()函数
+ * dns _error()函数
  *
  * Usage:
- *	报告使用unix I/O进行输入输出是的错误
+ *	报告DNS解析错误信息
  *
  * Return:
  *   0-无返回值，打印错误信息
  *
  */
-void dns_error(char *msg) /* DNS-style error */
+void dns_error(char *msg) 
 {
 	fprintf(stderr, "%s: DNS error %d\n", msg, h_errno);
 	exit(0);
 }
+
+/*
+ * 
+ * app_error()函数
+ *
+ * Usage:
+ *	报告应用运行错误信息
+ *
+ * Return:
+ *   0-无返回值，打印错误信息
+ *
+ */
+void app_error(char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	exit(0);
+}
+
 
 /* $end err_handle_wra.c */
