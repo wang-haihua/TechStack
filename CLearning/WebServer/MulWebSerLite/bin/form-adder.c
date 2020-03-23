785ff5f16ddaa5b48d0b20ec36784fc64d03d162
heverwong@163.com <heverwong@163.com>
2020-03-23 22:51:09 +0800

    Update form-adder.c

--- CLearning/WebServer/MulWebSerLite/bin/form-adder.c
+++ CLearning/WebServer/MulWebSerLite/bin/form-adder.c
@@ -7,38 +7,27 @@ int main(void) {
     int n1=0, n2=0;
 	char *method;
 
-    /* Extract the two arguments */
+    /* 根据CGI环境变量读取参数 */
     if ((buf = getenv("QUERY_STRING")) != NULL) {
-	p = strchr(buf, '&');
-	*p = '\0';
-	/*
-	strcpy(arg1, buf);
-	strcpy(arg2, p+1);
-	n1 = atoi(arg1);
-	n2 = atoi(arg2);
-	*/
-		/* 通过浏览器输入参数 */
+		p = strchr(buf, '&');
+		*p = '\0';
 		sscanf(buf, "first = %d", &n1);
 		sscanf(p+1, "second = %d", &n2);
     }
-
 	method = getenv("REQUEST_METHOD");
 
-    /* Make the response body */
-    sprintf(content, "Welcome to add.com: ");
-    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", 
-	    content, n1, n2, n1 + n2);
+    /* 创建HTTP响应主体 */
+    sprintf(content, "Welcome to WangHaihua‘s WebServer: ");
+    sprintf(content, "%sThese's the dynamic content service based on CGI\r\n<p>", content);
+    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
     sprintf(content, "%sThanks for visiting!\r\n", content);
-  
-    /* Generate the HTTP response */
+    
+    /* 形成HTTP响应 */
     printf("Content-length: %d\r\n", (int)strlen(content));
     printf("Content-type: text/html\r\n\r\n");
-    //printf("%s", content);
     if(strcasecmp(method, "HEAD") !=0 )
 		printf("%s", content);
-
 	fflush(stdout);
     exit(0);
-}
+}	
 /* $end adder */
